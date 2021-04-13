de21c10e08011dd02f799175cface2c9f28e7454
Doomsdan <samada5601@gmail.com>
2021-04-13 19:26:22 +0200

    first draft

---
+++ ha2/myNewton.m
@@ -0,0 +1,109 @@
+function [xZero,abortflag, iters] = myNewton(varargin)
+%%do the vcarargin
+for i = 1:nargin
+    if strcmp(varargin{i},'Function')
+        func = varargin{i+1};
+    elseif strcmp(varargin{i},'Derivative')
+        dfunc = varargin{i+1};
+    elseif strcmp(varargin{i},'StartValue')
+        x0 = varargin{i+1};
+    elseif strcmp(varargin{i},'MaxIter')
+        maxIter = varargin{i+1};
+    elseif strcmp(varargin{i},'Feps')
+        feps = varargin{i+1};
+    elseif strcmp(varargin{i},'Xeps')
+        xeps = varargin{i+1};
+    elseif strcmp(varargin{i},'LivePlot')
+        livePlot = varargin{i+1};
+    end
+end
+
+
+
+
+%% check for neccecssary parameters
+if ~exist('func','var')
+    error('No valid function');
+elseif ~exist('dfunc','var')
+    error('No valid derivative');
+end
+
+
+if ~exist('x0','var')
+    x0 = 0;
+    disp(['Using default startvalue: x0 = ',num2str(x0)]);  
+end
+
+if ~exist('maxIter','var')
+    maxIter = 50;
+    disp(['Using default MaxIter: maxIter = ',num2str(maxIter)]);  
+end
+
+if ~exist('xeps','var')
+    xeps = 1e-6;
+    disp(['Using default Xeps: xeps = ',num2str(xeps)]);  
+end
+
+if ~exist('feps','var')
+    feps = 1e-6;
+    disp(['Using default Feps: feps = ',num2str(feps)]);  
+end
+
+if ~exist('livePlot','var')
+    livePlot = 'off';
+    disp(['Using default Feps: feps = ',num2str(feps)]);  
+end
+
+
+
+
+
+%% start of algorithm
+if strcmp(livePlot,'on')
+    h = figure('name','Newton Visualisierung');
+    ax1 = subplot(2,1,1);
+    plot(ax1,0 ,x0 ,'bo');
+    ylabel('xvalue');
+    hold on;
+    grid on;
+    xlim('auto');
+    ax2 = subplot(2,1,2);
+    semilogy(ax2,0,x0,'rx');
+    ylabel('Function Value');
+    hold on;
+    grid on;
+    xlim('auto');
+    ylim('auto');
+
+end
+
+
+xOld = x0;
+abortflag = 'maxIter';
+for i=1:maxIter
+    f = func(xOld);
+    if f  < feps
+        abortflag = 'feps';
+        break;
+    end
+    df = dfunc(xOld);
+    if df == 0
+        abortflag = 'df is 0';
+        break;
+    end
+    xNew = xOld - f/df;
+    if abs(xNew-xOld) < xeps
+        abortflag = 'xeps';
+        break;
+    end
+    xOld = xNew;
+    if strcmp(livePlot,'on')
+       plot(ax1,i,xNew,'bo');
+       semilogy(ax2,i,func(xNew),'rx');
+       pause(0.1);
+        
+    end
+end
+xZero = xNew;
+iters = i;
+end
